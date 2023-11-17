8a1a5c2045680802f1e4f1d05c0455031d3b8bcf
Ewan132 <ewan132@yandex.ru>
2023-11-17 18:36:09 +0300

    Reformat code

--- src/main/java/ServiceCalc.java
+++ src/main/java/ServiceCalc.java
@@ -5,7 +5,7 @@ public class ServiceCalc {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expenses) /3;
+                money = (money - expenses) / 3;
             } else {
                 money = money + income - expenses;
             }
--- src/test/java/ServiceCalcTest.java
+++ src/test/java/ServiceCalcTest.java
@@ -8,7 +8,7 @@ public class ServiceCalcTest {
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/weekend.csv")
-    public void dayWeek(int income, int expenses, int threshold, int expected){
+    public void dayWeek(int income, int expenses, int threshold, int expected) {
         ServiceCalc calc = new ServiceCalc();
         int actual = calc.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
